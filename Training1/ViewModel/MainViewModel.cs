d72474604cdf37e0907e14e29ffce6213d81c1d6
Sascha Böck <wi16b142@technikum-wien.at>
2017-12-26 14:33:37 +0100

    edit xaml

---
+++ Training1/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+using GalaSoft.MvvmLight;
+
+namespace Training1.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            //to be done
+        }
+    }
+}
