19a756e4aa18671bff670c4a911147987d40fca7
Sascha Böck <wi16b142@technikum-wien.at>
2017-12-26 15:59:40 +0100

    create server

---
+++ Training1/Classes/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Training1.Classes
+{
+    public class Server
+    {
+        private Socket serverSocket, clientSocket;
+        public Server()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Loopback, 8055));
+            serverSocket.Listen(10);
+            clientSocket = serverSocket.Accept();
+        }
+    }
+}
--- Training1/ViewModel/MainViewModel.cs
+++ Training1/ViewModel/MainViewModel.cs
@@ -1,12 +1,14 @@
 using GalaSoft.MvvmLight;
+using Training1.Classes;
 
 namespace Training1.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        Server server;
         public MainViewModel()
         {
-            //to be done
+             server = new Server();
         }
     }
 }
